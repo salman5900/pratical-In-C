f5b282e2f8c2284b68ec92b867dc6eea5779530a
Salman Ahamed <salmanahamed5900@gmail.com>
2025-03-19 17:08:14 +0530

    new file questians added

---
+++ 11files.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    char ch[100];
+
+    // Open file in write mode
+    fp = fopen("file.txt", "w");
+    if (fp == NULL) {
+        printf("Error opening file\n");
+        return 1;
+    }
+
+    // Writing to the file
+    fprintf(fp, "Hello, World!");
+    fclose(fp);  // Close the file after writing
+
+    // Reopen the file in read mode
+    fp = fopen("file.txt", "r");
+    if (fp == NULL) {
+        printf("Error opening file for reading\n");
+        return 1;
+    }
+
+    // Reading from file
+    while (fgets(ch, sizeof(ch), fp) != NULL) {
+        printf("%s", ch);
+    }
+
+    fclose(fp);  // Close the file after reading
+
+    // Appending new data to the file
+    fp = fopen("file.txt", "a");
+    if (fp == NULL) {
+        printf("Error opening file for appending\n");
+        return 1;
+    }
+    fprintf(fp, "\nThis is appended data.");
+    fclose(fp);  // Close the file after appending
+    fp = fopen("file.txt", "r");
+     // Reading from file
+     while (fgets(ch, sizeof(ch), fp) != NULL) {
+        printf("%s", ch);
+    }
+    fclose(fp);  // Close the file after reading
+    return 0;
+}
---
+++ 12files.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    // writing into a file
+    fp = fopen("file.txt", "w");
+    if (fp == NULL) {
+        printf("Error opening file\n");
+        return 1;
+    }
+    fprintf(fp, "Hello, i am writing content into this file\n");
+    fprintf(fp, "This is the second line\n");
+    fclose(fp);
+
+    // reading from a file and take count of characters
+    int count = 0;
+    char c;
+    fp = fopen("file.txt", "r");
+    if (fp == NULL) {
+        printf("Error opening file\n");
+        return 1;
+    }
+    while ((c = fgetc(fp))!= EOF)
+        count++;
+    fclose(fp);
+    printf("Total number of characters in the file: %d\n", count);
+
+    // reading from a file and take count of words
+
+    int words = 0;
+    fp = fopen("file.txt", "r");
+    if (fp == NULL) {
+        printf("Error opening file\n");
+        return 1;
+    }
+    char buffer[100];
+    while (fscanf(fp, "%s", buffer) != EOF) {
+        words++;
+    }
+    fclose(fp);
+    printf("Total number of words in the file: %d\n", words);
+
+    // reading from a file and take count of lines
+    int lines = 0;
+    fp = fopen("file.txt", "r");
+    if (fp == NULL) {
+        printf("Error opening file\n");
+        return 1;
+    }
+    while ((c = fgetc(fp))!= EOF) {
+        if (c == '\n'){
+            lines++;
+        }
+    }
+    fclose(fp);
+    printf("Total number of lines in the file: %d\n", lines);
+    return 0;
+}
